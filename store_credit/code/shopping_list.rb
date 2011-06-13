a71bc46d07216471b68bed639e2f78eda99decb6
charliequinn <charlesquinn1984@gmail.com>
2011-06-13 13:59:29 +0100

    refactored shopping_list.rb

--- store_credit/code/shopping_list.rb
+++ store_credit/code/shopping_list.rb
@@ -1,68 +1,68 @@
 class ShoppingList
-	def self.create_shopping_list(filename)
-		results_file = "results_for_#{filename}"
-		
-		file_lines = ShoppingList.parse_file(filename)
-		test_cases = ShoppingList.extract_test_cases(file_lines)		
-		
-		File.open(results_file,'w') do |file|
-			count = 1
-			test_cases.each do |test_case|
-				result = ShoppingList.pick_two_items_to_buy(test_case)		
-				file.puts("Case ##{count}: #{result[0]} #{result[1]}")
-				count += 1
-			end		
-		end
+  def self.create_shopping_list(filename)
+    results_file_name = "results_for_#{filename}"
 
-		return results_file
-	end
-	
-	def self.parse_file(filename)
-		file_lines = []
-		File.open(filename, 'rb') do |file|
-			file.each_line do |line|
-				file_lines << line.gsub(/\r\n?/, "")
-			end
-		end
-		
-		file_lines
-	end
-	
-	def self.pick_two_items_to_buy(test_case)
-		store_credit = test_case[0].to_i
-		total_items = test_case[1].to_i 
-		item_values = test_case[2].split(" ")
+    file_lines = ShoppingList.parse_file(filename)
+    test_cases = ShoppingList.extract_test_cases(file_lines)
 
-		i = 0
-		while(i < item_values.length)
-			item_value = item_values[i].to_i			
-			value_to_find = store_credit - item_value
-			
-			remainder_of_item_values = item_values[i+1..item_values.length-1]
-			
-			second_item_index = remainder_of_item_values.index(value_to_find.to_s)
-			
-			if !second_item_index.nil?
-				index_of_item_offset = i + 1
-				second_item_index_offset = second_item_index + 1			
-				return ["#{index_of_item_offset}","#{index_of_item_offset+second_item_index_offset}"] if !second_item_index.nil?
-			end
-			
-			i += 1
-		end		
-	end
-	
-	def self.extract_test_cases(file_lines)
-		test_cases = []
-		
-		i = 1
-		while(i < file_lines.length)
-			upper_limit = i + 2
-			test_cases << file_lines[i..upper_limit]
-			i += 3
-		end
-		
-		test_cases
-	end
-	
+    return create_results_file(results_file_name, test_cases)
+  end
+
+  def self.parse_file(filename)
+    file_lines = []
+    IO.readlines(filename).each do |line|
+      file_lines << line.gsub(/\n?/, "")
+    end
+    file_lines
+  end
+
+  def self.extract_test_cases(file_lines)
+    test_cases = []
+
+    i = 1
+    while (i < file_lines.length)
+      upper_limit = i + 2
+      test_cases << file_lines[i..upper_limit]
+      i += 3
+    end
+
+    test_cases
+  end
+
+  def self.create_results_file(results_filename, test_cases)
+    File.open(results_filename, 'w') do |file|
+      count = 1
+      test_cases.each do |test_case|
+        result = ShoppingList.pick_two_items_to_buy(test_case)
+        file.puts("Case ##{count}: #{result[0]} #{result[1]}")
+        count += 1
+      end
+    end
+
+    return results_filename
+  end
+
+  def self.pick_two_items_to_buy(test_case)
+    store_credit = test_case[0].to_i
+    total_items = test_case[1].to_i
+    item_values = test_case[2].split(" ")
+
+
+    item_values.each_index do |i|
+      item_value = item_values[i].to_i
+      value_to_find = store_credit - item_value
+
+      remainder_of_item_values = item_values[i+1..total_items-1]
+
+      second_item_index = remainder_of_item_values.index(value_to_find.to_s)
+
+      return prepare_and_format_result(i, second_item_index) if !second_item_index.nil?
+    end
+  end
+
+  def self.prepare_and_format_result(i, second_item_index)
+    index_of_item_offset = i + 1
+    second_item_index_offset = second_item_index + 1
+    return ["#{index_of_item_offset}", "#{index_of_item_offset+second_item_index_offset}"]
+  end
 end
--- store_credit/code/shopping_list_test.rb
+++ store_credit/code/shopping_list_test.rb
@@ -1,74 +1,75 @@
 require 'shopping_list'
 
 class ShoppingListTest
-	def self.overall_test
-		expected_results = IO.readlines('expected_test_results.txt')
-		result_file_name = ShoppingList.create_shopping_list('test_input.txt')
-		results =  IO.readlines(result_file_name)
-		
-		expected_results == results
-	end
-	
-	def self.test_parse_file()
-		file_to_read = 'test_input.txt'
-		expected_file_lines = [	'3',
-						'100',
-						'3',
-						'5 75 25',
-						'200',
-						'7',
-						'150 24 79 50 88 345 3',
-						'8',
-						'8',
-						'2 1 9 4 4 56 90 3']
+  def self.overall_test
+    expected_results = IO.readlines('expected_test_results.txt')
+    result_file_name = ShoppingList.create_shopping_list('test_input.txt')
+    results =  IO.readlines(result_file_name)
 
-		result = ShoppingList.parse_file(file_to_read)
-		expected_file_lines == result
-	end
-	
-	def self.test_pick_two_items_to_buy_a
-		test_case = [	'100',
-					'3',
-					'5 75 25']
-					
-		expected_result = ['2','3']
+    expected_results == results
+  end
 
-		result = ShoppingList.pick_two_items_to_buy(test_case)
-		expected_result == result
-	end
-	
-	def self.test_pick_two_items_to_buy_b
-		test_case = [	'200',
-					'7',
-					'24 150 79 50 88 345 3']
-					
-		expected_result = ['2','4']
+  def self.test_parse_file()
+    file_to_read = 'test_input.txt'
+    expected_file_lines = ['3',
+                           '100',
+                           '3',
+                           '5 75 25',
+                           '200',
+                           '7',
+                           '150 24 79 50 88 345 3',
+                           '8',
+                           '8',
+                           '2 1 9 4 4 56 90 3']
 
-		result = ShoppingList.pick_two_items_to_buy(test_case)
+    result = ShoppingList.parse_file(file_to_read)
 
-		expected_result == result
-	end
+    expected_file_lines == result
+  end
 
-	def self.test_extract_test_cases
-		file_lines = ['3',
-				'100',
-				'3',
-				'5 75 25',
-				'200',
-				'7',
-				'150 24 79 50 88 345 3',
-				'8',
-				'8',
-				'2 1 9 4 4 56 90 3']
-				
-		expected_result = [['100','3','5 75 25'],
-					  ['200','7','150 24 79 50 88 345 3'],
-					  ['8','8','2 1 9 4 4 56 90 3']]
+  def self.test_pick_two_items_to_buy_a
+    test_case = ['100',
+                 '3',
+                 '5 75 25']
 
-		result = ShoppingList.extract_test_cases(file_lines)
+    expected_result = ['2', '3']
 
-		expected_result == result
-	end
+    result = ShoppingList.pick_two_items_to_buy(test_case)
+    expected_result == result
+  end
+
+  def self.test_pick_two_items_to_buy_b
+    test_case = ['200',
+                 '7',
+                 '24 150 79 50 88 345 3']
+
+    expected_result = ['2', '4']
+
+    result = ShoppingList.pick_two_items_to_buy(test_case)
+
+    expected_result == result
+  end
+
+  def self.test_extract_test_cases
+    file_lines = ['3',
+                  '100',
+                  '3',
+                  '5 75 25',
+                  '200',
+                  '7',
+                  '150 24 79 50 88 345 3',
+                  '8',
+                  '8',
+                  '2 1 9 4 4 56 90 3']
+
+    expected_result = [['100', '3', '5 75 25'],
+                       ['200', '7', '150 24 79 50 88 345 3'],
+                       ['8', '8', '2 1 9 4 4 56 90 3']]
+
+    result = ShoppingList.extract_test_cases(file_lines)
+
+    expected_result == result
+  end
 end
 
 puts "overall_test -- #{ShoppingListTest.overall_test} --"
